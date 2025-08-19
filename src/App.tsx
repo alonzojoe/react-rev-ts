275bf46a5b941ed95a467994c70a0c57727a35bd
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-19 16:49:44 +0800

    create and style base layer of Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="w-full flex flex-col">
       <Navbar />
+      <Header />
     </div>
   );
 }
