55873bfae1ff367cc76130fc2686ed56151da5cf
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-20 10:23:11 +0800

    crate new Partners component import and implement in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Partners from "./components/Partners";
 function App() {
   return (
     <div className="w-full flex flex-col">
       <Navbar />
       <Header />
+      <Partners />
     </div>
   );
 }
