83e45d210f29579eda06ce3f38e765e028b2aa2c
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-20 13:29:16 +0800

    styling SectionHeader and implement in Features component iterate each items of constants FEATURES Items.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,5 +1,30 @@
+import SectionHeader from "./SectionHeader";
+import { HEADER_CONTENT } from "../constants";
+import { FEATURES } from "../constants";
 const Features = () => {
-  return <div className="text-white">Features</div>;
+  return (
+    <div className="w-full flex flex-col relative pt-[80px] gap-[80px] md:px-[120px]">
+      <SectionHeader
+        nowrap
+        tag={HEADER_CONTENT["Features"].tag}
+        title={HEADER_CONTENT["Features"].title}
+        description={HEADER_CONTENT["Features"].description}
+      />
+      <div className="w-full flex md:flex-row flex-col items-center justify-center gap-[24px]">
+        {FEATURES.map((f, index) => (
+          <div className="w-fit px-[50px] rounded-[12px] bg-white/4 border border-grayborder py-[50px] flex flex-col gap-[35px] items-center justify-center">
+            <img src={f.icon} alt={`${f.title}-index`} key={index} />
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <span className="text-light text-[20px]">{f.title}</span>
+              <p className="text-gray1 max-w-[310px] text-center">
+                {f.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -8,9 +8,25 @@ const SectionHeader = ({
   tag,
   title,
   description,
-  nowrap,
+  nowrap = false,
 }: SectionHeaderProps) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center">
+      <span className="text-primary px-3 min-w-[100px] h-[41px] rounded-[24px] border border-gray3 flex items-center justify-center text-sm">
+        {tag}
+      </span>
+      <span
+        className={`text-light md:text-[50px] text-[35px] max-w-[900px] text-center ${
+          nowrap ? "md:whitespace-nowrap" : "whitespace-normal"
+        }`}
+      >
+        {title}
+      </span>
+      <p className="text-center md:max-w-[640px] max-w-[90%] text-gray1 text-lg">
+        {description}
+      </p>
+    </div>
+  );
 };
 
 export default SectionHeader;
