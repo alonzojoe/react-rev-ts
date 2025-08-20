0ad05f520a599a6ed673b7bd7ce26be4f6600ddd
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-20 10:19:03 +0800

    finalized Header UI UX

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import HomeImg from "../assets/images/imgRight.svg";
+
 const Header = () => {
   return (
     <div className="w-full flex md:flex-row flex-col items-center justify-between md:px-[120px] px-5 md:h-[calc(100vh-80px)] relative md:pb-0 pb-20">
@@ -13,7 +15,22 @@ const Header = () => {
           Secure scalable and decentralized solutions for your digital
           assets-experience the future of financial freedom
         </p>
+        <div className="flex items-center gap-5">
+          <button className="bg-primary text-black text-center cursor-pointer rounded-[40px] w-[150px] h-[50px]">
+            Get Started
+          </button>
+          <button className="text-light border border-grayborder text-center cursor-pointer rounded-[40px] w-[190px] h-[50px]">
+            Watch video
+          </button>
+        </div>
       </div>
+      <img
+        className="mt-20 md:mt-0 md:w-auto w-[400px]"
+        src={HomeImg}
+        alt="Home-Img"
+        loading="lazy"
+      />
+      <div className="absolute top-1/2 left-[200px] -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-overlay/10 blur-3xl"></div>
     </div>
   );
 };
