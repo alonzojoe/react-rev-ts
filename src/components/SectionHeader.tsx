40b14b11eeac145697e83b2960de812e6be80cbd
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-20 11:04:36 +0800

    create new SectionHeader component and define props type created an interface for the object shape

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,16 @@
+interface SectionHeaderProps {
+  tag: string;
+  title: string;
+  description: string;
+  nowrap?: boolean;
+}
+const SectionHeader = ({
+  tag,
+  title,
+  description,
+  nowrap,
+}: SectionHeaderProps) => {
+  return <div></div>;
+};
+
+export default SectionHeader;
