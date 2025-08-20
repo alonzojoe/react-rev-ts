67493bcc0d36216ff78a1e157bfce3a6d674875d
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-20 10:38:06 +0800

    styling UI UX of Partners component

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import { PARTNERS } from "../constants";
 const Partners = () => {
-  return <div>Partners</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-[40px] py-[40px] bg-gray4">
+      <span className="text-gray1">Satisfied Partners</span>
+      <div className="w-full flex md:flex-row flex-col items-center justify-center gap-[72px]">
+        {PARTNERS.map((p, index) => (
+          <img src={p} alt={`partners-${index + 1}`} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
