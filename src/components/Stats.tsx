acb62dd09521d93fc9d0f17e4a78a8fc70a0b85e
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-21 10:22:05 +0800

    Stats component implement SectionHeader apply constant: HEADER_CONTENT Stats property

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import SectionHeader from "./SectionHeader";
+import { HEADER_CONTENT } from "../constants";
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="w-full flex flex-col relative gap-[80px] md:py-[120px] py-[80px] md:px-[120px] bg-gray4">
+      <SectionHeader
+        nowrap
+        tag={HEADER_CONTENT["Stats"].tag}
+        title={HEADER_CONTENT["Stats"].title}
+        description={HEADER_CONTENT["Stats"].description}
+      />
+    </div>
+  );
 };
 
 export default Stats;
