8daaef3c853814a853d784aa7f030f7dffb73a09
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-21 14:37:43 +0800

    styling Testimonial component implement SectionHeader and constant HEADER_CONTENT for Testimonial property

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,5 +1,19 @@
+import SectionHeader from "./SectionHeader";
+import { HEADER_CONTENT } from "../constants";
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <div className="w-full flex flex-col gap-[80px] relative py-[60px] bg-gray4">
+      <SectionHeader
+        nowrap
+        tag={HEADER_CONTENT["Testimonial"].tag}
+        title={HEADER_CONTENT["Testimonial"].title}
+        description={HEADER_CONTENT["Testimonial"].description}
+      />
+      <div className="w-full flex flex-col gap-4 overflow-hidden">
+        
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
