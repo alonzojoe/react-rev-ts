e767a449e2f880392b20df80571434ad20593ae4
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-22 15:33:12 +0800

    implement SectionHeader for FAQ property create new state openIndexes and toggle item function modifier

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import { useState } from "react";
+import SectionHeader from "./SectionHeader";
+import { HEADER_CONTENT } from "../constants";
 const Faq = () => {
-  return <div>Faq</div>;
+  const [openIndexes, setOpenIndexes] = useState<number[]>([]);
+
+  const toggleItem = (index: number) => {
+    setOpenIndexes((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
+
+  return (
+    <div className="w-full flex flex-col gap-[80px] relative py-[60px] md:px-[120px]">
+      <SectionHeader
+        nowrap
+        tag={HEADER_CONTENT["FAQ"].tag}
+        title={HEADER_CONTENT["FAQ"].title}
+        description={HEADER_CONTENT["FAQ"].description}
+      />
+    </div>
+  );
 };
 
 export default Faq;
