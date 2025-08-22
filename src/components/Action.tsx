0ff68314c0a23cf7a8a89eedeb7bd7e731260298
alonzojoe <alonzojoenell2k15@gmail.com>
2025-08-22 23:43:57 +0800

    implement SectionHeader with HEADER_CONTENT Action property: tag,title and description

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -1,5 +1,16 @@
+import SectionHeader from "./SectionHeader";
+import { HEADER_CONTENT } from "../constants";
 const Action = () => {
-  return <div></div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-5 relative py-[80px] md:px-[120px] bg-gray4">
+      <SectionHeader
+        nowrap={false}
+        tag={HEADER_CONTENT["Action"].tag}
+        title={HEADER_CONTENT["Action"].title}
+        description={HEADER_CONTENT["Action"].description}
+      />
+    </div>
+  );
 };
 
 export default Action;
